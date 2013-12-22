ea1d14b290604b77a97727565920c32db0e5bfba
John Ong <johnmanong@gmail.com>
2013-12-22 13:38:54 -0500

    ch 5.3.4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,68 +3,68 @@ require 'spec_helper'
 describe "Static pages" do
   let(:title_base){ "Ruby on Rails Tutorial Sample  App" } 
 
+  subject { page }
+
   # Home Page tests
   describe "Home page" do
-  	# ensure title copy is correct
-  	it "should have the content 'Sample App'" do 		# description
-  		visit root_path						                    # action
-  		expect(page).to have_content('Sample App')		# requirement
-
-    end
-    it "should have base title" do 
-    	visit root_path
-  		expect(page).to have_title("#{title_base}") # will also do substring match
-  	end
-
-    it "should not have a custom page title" do
+    before {
       visit root_path
-      expect(page).not_to have_title(" | Home")
-    end
+    }
+
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title(" | Home") }
+
+  	# # ensure title copy is correct
+  	# it "should have the content 'Sample App'" do 		# description
+  	# 	expect(page).to have_content('Sample App')		# requirement
+   #  end
+   #  it "should have base title" do 
+  	# 	expect(page).to have_title("#{title_base}") # will also do substring match
+  	# end
+
+   #  it "should not have a custom page title" do
+   #    expect(page).not_to have_title(" | Home")
+   #  end
   end
 
   # Help Page tests
   describe "Help Page" do
-  	# ensure help appears in copy
-  	it "shoud have the content 'Help'" do
-  		visit help_path
-  		expect(page).to have_content('Help')
-  	end
-  	it "should have the title 'Help'" do 
-    	visit help_path
-  		expect(page).to have_title("#{title_base} | Help") # will also do substring match
-  	end
+    before {
+      visit help_path
+    }
+
+    # ensure help appears in copy
+    it { should have_content('Help') }
+		it { should have_title("#{title_base} | Help") } # will also do substring match
   end
 
   # About page tests
   describe "About page" do
-  	it "should have content 'About Us'" do
-  		visit about_path
-  		expect(page).to have_content('About Us')
-  	end
+    before {
+      visit about_path
+    }
+
+    it { should have_content('About Us') }
+    it { should have_title("#{title_base} | About") } # will also do substring match
+    
+
   # 	it "should have valid tutorial link" do 
 		# tutorial_link = find(:css, "a:contains('Tutorial')")
   # 		tutorial_link.click
   # 		get page status, should be (200) or simply check
   # 	end
   	
-    it "should have the title 'About'" do 
-    	visit about_path
-  		expect(page).to have_title("#{title_base} | About") # will also do substring match
-  	end
   end
 
   #test for contact page (exercise)
   describe "Contact page" do
-  	it "should have the title 'Contact'" do
-  		visit contact_path
-  		expect(page).to have_title("#{title_base} | Contact") #can also substring match
-  	end
-
-    it "it should have content 'Contact'" do
+    before {
       visit contact_path
-      expect(page).to have_content('Contact')
-    end
+    }
 
+    it { should have_title("#{title_base} | Contact") } #can also substring match
+    it { should have_content('Contact') }
 
   end
 
