b5c50ef8580395000c761ac6e28c4bea8f749ed2
John Ong <johnmanong@gmail.com>
2013-12-22 05:13:57 -0500

    ch 5.3.3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,17 +7,17 @@ describe "Static pages" do
   describe "Home page" do
   	# ensure title copy is correct
   	it "should have the content 'Sample App'" do 		# description
-  		visit '/static_pages/home'						# action
+  		visit root_path						                    # action
   		expect(page).to have_content('Sample App')		# requirement
 
     end
     it "should have base title" do 
-    	visit '/static_pages/home'
+    	visit root_path
   		expect(page).to have_title("#{title_base}") # will also do substring match
   	end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title(" | Home")
     end
   end
@@ -26,11 +26,11 @@ describe "Static pages" do
   describe "Help Page" do
   	# ensure help appears in copy
   	it "shoud have the content 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		expect(page).to have_content('Help')
   	end
   	it "should have the title 'Help'" do 
-    	visit '/static_pages/help'
+    	visit help_path
   		expect(page).to have_title("#{title_base} | Help") # will also do substring match
   	end
   end
@@ -38,7 +38,7 @@ describe "Static pages" do
   # About page tests
   describe "About page" do
   	it "should have content 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		expect(page).to have_content('About Us')
   	end
   # 	it "should have valid tutorial link" do 
@@ -48,7 +48,7 @@ describe "Static pages" do
   # 	end
   	
     it "should have the title 'About'" do 
-    	visit '/static_pages/about'
+    	visit about_path
   		expect(page).to have_title("#{title_base} | About") # will also do substring match
   	end
   end
@@ -56,12 +56,12 @@ describe "Static pages" do
   #test for contact page (exercise)
   describe "Contact page" do
   	it "should have the title 'Contact'" do
-  		visit '/static_pages/contact'
+  		visit contact_path
   		expect(page).to have_title("#{title_base} | Contact") #can also substring match
   	end
 
     it "it should have content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
