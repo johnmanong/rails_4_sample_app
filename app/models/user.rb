7d778bb00718518ace108bce1d4c9646a30ae0ed
johnmanong <johnmanong@gmail.com>
2013-12-29 01:26:39 -0500

    ch 7.4.2 working form, strong params, error messages, flash

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ActiveRecord::Base
   }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  # validates(:name, presence: true)
   validates :name,  presence: true,
                     length: { maximum: 50 }
   validates :email, presence: true,
