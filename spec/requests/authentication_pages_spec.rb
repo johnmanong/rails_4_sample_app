2a541ad3fe06e272a7a4bf1264148113ade453d3
johnmanong <johnmanong@gmail.com>
2014-01-02 00:07:59 -0500

    ch 8.1.3

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -6,7 +6,31 @@ describe "AuthenticationPages" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_title("Sign in") }
-    it { should have_content("Sign in") }
+    let(:signin_btn)  { "Sign in" }
+
+    describe "with invalid info" do
+      before do
+        click_button signin_btn
+      end
+
+      it { should have_title("Sign in") }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    end
+
+    describe "with valid info" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      before do
+        fill_in "Email", with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button signin_btn
+      end
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+    end
+
   end
 end
